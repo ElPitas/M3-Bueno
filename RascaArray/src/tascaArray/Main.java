04f2b4cfd1e549bfef3901da4e09b80f9bde28a7
cfgs <cfgs@pc-info-host>
2026-02-02 18:46:16 +0100

    COLA2

--- RascaArray/src/tascaArray/Main.java
+++ RascaArray/src/tascaArray/Main.java
@@ -6,11 +6,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*
+		
 		ArrayList<Persona> array = new ArrayList<>();
-			Pacient p0 = new Pacient("Nyx", "89746513E");
-			Pacient p1 = new Pacient("Orfeo", "56468143A");
-			Doctor p2 = new Doctor("Izanami", "41321654W", "1000 verdades");
-			Doctor p3 = new Doctor("Arsene", "84981325C", "LeBlank");
+			Persona p0 = new Persona("Nyx", "89746513E");
+			Persona p1 = new Persona("Orfeo", "56468143A");
+			Persona p2 = new Persona("Izanami", "41321654W");
+			Persona p3 = new Persona("Arsene", "84981325C");
 			array.add(p0);
 			array.add(p1);
 			array.add(p2);
@@ -24,6 +26,26 @@ public class Main {
 			for (Persona element : array) {
 				System.out.print(element + " ");
 			}	
+			*/
+		
+		
+		ArrayList<Persona> array2 = new ArrayList<>();
+			Pacient p0 = new Pacient("Nyx", "89746513E");
+			Pacient p1 = new Pacient("Orfeo", "56468143A");
+			Doctor p2 = new Doctor("Izanami", "41321654W", "1000 verdades");
+			Doctor p3 = new Doctor("Arsene", "84981325C", "LeBlank");
+		
+			for (Persona p : array2) {
+				if(p instanceof Doctor) {
+					Doctor pac = (Doctor)p;
+					System.out.println("ii");
+				}else {
+					System.out.println("ii");
+				}
+			}	
+			 System.out.println("A");
+		
+			
 		}
 
 }
